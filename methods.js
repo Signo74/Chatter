a240c769f6e83dfc124957456b23b31dafc8b37c
Victor Marinov <victor.v.marinov@gmail.com>
2015-11-03 17:47:30 +0200

    Time stored for messages
    Small update to the messages document where the time of the message is
    persisted in HH:MM:SS format

--- methods.js
+++ methods.js
@@ -13,22 +13,43 @@ Meteor.methods({
   },
   updateChatRoomMessages: function(message, roomId) {
     var date = new Date();
-    var formattedDate = '';
+    var formattedDate = formatDate();
+    var formattedTime = formatTime();
     var username = 'anonymous';
-
-    formattedDate += date.getDate();
-    formattedDate += '-';
-    formattedDate += date.getMonth();
-    formattedDate += '-';
-    formattedDate += date.getFullYear();
-
-    console.log(Meteor.user());
 
     if (Meteor.user() != null) {
       username = Meteor.user().emails[0].address;
       username = username.slice(0, username.indexOf('@'));
     }
 
-    Messages.insert({date: formattedDate, username: username, message: message, room: roomId});
+    Messages.insert({date: formattedDate, time: formattedTime, username: username, message: message, room: roomId});
   }
 })
+
+// Parses a new Date() object into DD-MM-YYYY string format.
+function formatDate() {
+  var formattedDate = '';
+  var date = new Date();
+
+  formattedDate += date.getDate();
+  formattedDate += '-';
+  formattedDate += date.getMonth();
+  formattedDate += '-';
+  formattedDate += date.getFullYear();
+
+  return formattedDate;
+}
+
+// Parses a new Date() object into HH:MM:SS string format.
+function formatTime() {
+  var formattedDate = '';
+  var date = new Date();
+
+  formattedDate += date.getHours();
+  formattedDate += ':';
+  formattedDate += date.getMinutes();
+  formattedDate += ':';
+  formattedDate += date.getSeconds();
+
+  return formattedDate;
+}
